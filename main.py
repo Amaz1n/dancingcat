f102d379e7c0c39f45c6ad31a2cbb70e91d43242
Richard Zhou <richardzhou55@gmail.com>
2023-06-26 11:08:12 -0400

    Fixed window crash on Exit button, simplified code

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
-# This is a sample Python script.
+# Cat doing a silly dance
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import cv2 as cv
-import sys
+import cv2
 
 
+# Draw the frames
 def draw(images):
     i = 0
     j = 1
-    cv.imshow("danc cat", images[0])
-    while cv.getWindowProperty("danc cat", 0) >= 0:
-        cv.imshow("danc cat", images[i])
+    cv2.imshow("danc cat", images[0])
+    while True:
+        cv2.imshow("danc cat", images[i])
         i += j
 
         if (i <= 0 and j == -1) or (i >= len(images) - 1 and j == 1):
             j *= -1
 
-        cv.waitKey(200)
+        cv2.waitKey(200)
 
-# Press the green button in the gutter to run the script.
+        if cv2.getWindowProperty("danc cat", cv2.WND_PROP_VISIBLE) < 1:
+            break
+    cv2.destroyAllWindows()
+
+
+# Main program code
 if __name__ == '__main__':
     img_list = []
 
     for i in range(3):
-        img = cv.imread(cv.samples.findFile("cat" + str(i+1) + ".jpg"))
+        img = cv2.imread(cv2.samples.findFile("cat" + str(i+1) + ".jpg"))
 
         if img is not None:
             img_list.append(img)
 
     draw(img_list)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
